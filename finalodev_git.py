649c34cd9cc0014ad9319f0980270669ffa49cf3
ezgiozkaan <68017886+ezgiozkaan@users.noreply.github.com>
2020-07-09 16:43:21 +0300

    Add files via upload

---
+++ finalodev_git.py
@@ -0,0 +1,30 @@
+#soru4
+sayi1 = 1
+sayi2 = 10
+
+print(sayi1,"ile",sayi2,"arasındaki asal sayılar:")
+
+for sayi in range(sayi1,sayi2 + 1):
+   if sayi > 1:
+       for i in range(1,10):
+           if (sayi % i) == 0:
+               break
+       else:
+           print(sayi)
+
+#soru2
+
+sonuc = list(map(lambda x : x if 9 <= x < 16 : (2x) else 9 > x => 16 :(3x!)
+
+
+#soru3
+dizi1 = [ (5,26,7)
+          (9,15,26)
+          (11,1,14)]
+dizi2 = [(1,2,-1)
+         (2,5,2)
+         (-1,-2,2)]
+dizi3 = np.dot(dizi1,dizi2)
+print(dizi3)
+print(/n)
+
